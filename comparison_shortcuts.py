ab98e2ee1f49550bf362f64bfc3fd4515358d468
J Martinez <javier.martinezrom@gmail.com>
2023-05-21 11:31:45 +0200

    Day 4

---
+++ comparison_shortcuts.py
@@ -0,0 +1,18 @@
+# Everything boils down to True or False statements
+# TODO: Try this with a set, a list, a tuple, all of them to try with both, empty and with items
+# These shortcuts are constantly used in programming
+
+something = None # None always returns false
+
+if something:
+    print("This is true")
+else:
+    print("This is false")
+# If we try this with a different value or a string
+
+something = "String used here" # If an empty string was to be used here that would turn it into False
+
+if something:
+    print("This is true")
+else:
+    print("This is false")
---
+++ comparison_shortcuts_ownexamples.py
@@ -0,0 +1,23 @@
+phone_brands = ["Nokia", "Samsung", "Motorola", "Apple"]
+
+if phone_brands:
+    print("We have it in stock")
+
+prices = ["3", "5", "8", "10",]
+
+if prices:
+    print("See below")
+
+else:
+    print("Not priced")
+
+for price in prices:
+    print("The prices are", price)
+
+stock = {""}
+
+if stock:
+    print("Stock coming soon")
+
+else:
+    print("Stock not expected soon")
