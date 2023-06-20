e0c5cc458d33b6669387160dfe6f334ce6851052
J Martinez <javier.martinezrom@gmail.com>
2023-06-20 21:39:12 +0200

    Day 29. Pyhon 301

---
+++ pipenvs.py
@@ -0,0 +1,15 @@
+# 'pipenv install' will install pipenv
+# 'pipenv show' will show a list of commands that can be used
+# 'pipenv env' in order to go inside a pip environment
+# One can use pipenv for a range of things
+# Pipenv to create an environment and work with Django (an older version)
+
+# First pipenv install Django==2.*
+# This will create a shell of Django that can be used
+# Using 'pip show Django' will tell you the version installed
+# in order to exit it, type: pipenv --rm
+
+# Same can be done for an older version of python
+# pipenv install --python 3.7 (this being an older version of python)
+# This will create a shell using a previous version of Python
+# in order to exit it, same as before. Type: pipenv --rm
